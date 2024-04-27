20a27e0bcfbb760bd7a0733adcf3120075e33e23
fitahw <shitass@int.pl>
2024-04-27 16:54:09 +0200

    working HTML form with AJAX request (no submit button)

--- funiWEB/clipper/views.py
+++ funiWEB/clipper/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'clipper.html')
+
+
+def ajaxData(request):
+    print('ajaxData')
--- funiWEB/urls.py
+++ funiWEB/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .clipper import views
+
+from funiWEB import clipper
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('ajaxData/', views.ajaxData, name='ajaxData'),
 ]
