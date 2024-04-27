f1bafdd12991e294f7f536c8cde2f591a8bbfae2
fitahw <shitass@int.pl>
2024-04-27 20:06:26 +0200

    working download through yt-dlp

--- funiWEB/clipper/views.py
+++ funiWEB/clipper/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
-
+from django.http import JsonResponse, FileResponse
+import yt_dlp
+import os
 # Create your views here.
 
 
@@ -10,4 +10,38 @@ def index(request):
 
 
 def ajaxData(request):
-    print('ajaxData')
+    if request.method == "POST":
+        url = request.POST.get('YTurl')
+        urlHash = url[32:]
+        ''''
+        with yt_dlp.YoutubeDL() as ydl:
+            info_dict = ydl.extract_info(url, download=False)
+            filenameDirt = info_dict.get('title', None)
+            filename = "_".join(filenameDirt.split())
+        '''
+
+        path = f'clips/{urlHash}.mp4'
+        ydl_opts = {
+            'outtmpl': path,
+            'format': 'b',
+            # 'download_ranges': download_range_func(None, [(time1, time2)]),
+            #'force_keyframes_at_cuts': True,
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }]
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                ydl.download(url)
+            except:
+                print('Error in downloading video')
+                return JsonResponse({'success': False})
+
+            if os.path.exists(f'clips/{urlHash}.mp4'):
+                return FileResponse(open(f'clips/{urlHash}.mp4', 'rb'), as_attachment=True, filename=f'{urlHash}.mp4')
+            else:
+                return JsonResponse({'success': False})
+    else:
+        return JsonResponse({'success': False})
