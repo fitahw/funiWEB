8eb81280b05abfedd2dc73b1f5222564ae9e29eb
fitahw <shitass@int.pl>
2024-04-27 11:58:13 +0200

    dir change

--- funiWEB/clipper/apps.py
+++ funiWEB/clipper/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ClipperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clipper'
+    name = 'funiWEB.clipper'
--- funiWEB/settings.py
+++ funiWEB/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clipper.apps.ClipperConfig',
+    'funiWEB.clipper.apps.ClipperConfig',
 ]
 
 MIDDLEWARE = [
